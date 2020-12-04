21f920b22d0a9730e62dcdf05d271cb34516ccbc
Jessi19Jassi <jessyhi44@hotmail.com>
2020-12-04 11:17:00 -0600

    Pantalla de inicio ttps://htmlcolorcodes.com/es/

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
-  // ...
+  // cifrado y descifrado
+  //Investigar objetos y parametros de las funciones
+  //El usuario debe definir los movimientos
+  //Propiedades (caracteristicas) y métodos de una función (qué va a hacer)
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function registra(){
+    document.getElementById('content').style.display='none'; //ocultar
+    caja_name.style.display='block';  //mostrar
+}
+
+function showname(){
+    let name;
+    name=document.getElementById("name").value
+    document.getElementById()
+}
+
+
 import cipher from './cipher.js';
 
 console.log(cipher);
