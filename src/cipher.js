bb07f1f7fd11d079dde991b4db2df735ad4651b0
Jessi19Jassi <jessyhi44@hotmail.com>
2020-12-21 23:17:55 -0600

    Cifrado y descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,60 @@
 const cipher = {
-  // cifrado y descifrado
-  //Investigar objetos y parametros de las funciones
-  //El usuario debe definir los movimientos
-  //Propiedades (caracteristicas) y métodos de una función (qué va a hacer)
-};
+
+  encode: function(desplazamiento, texto){
+      var resultado = "";
+   
+      if(desplazamiento == null){
+        throw new TypeError('Not Valid');
+      }
+      else if(texto == []){
+          throw new TypeError('Not Valid');
+        }
+        else if(desplazamiento == 0){
+          throw new TypeError('Not Valid');
+        }
+      
+
+    for(let i=0; i < texto.length; i++){ 
+      var text = texto.charCodeAt(i);  //Pasar texto a ASCII
+     if(text >= 65 && text <= 90) {
+        resultado += String.fromCharCode((text - 65 + desplazamiento) % 26 + 65); //fromCharCode: Convierte de ascii a string
+       
+     }else{
+          resultado += texto.charAt(i);
+        }
+    }
+    return resultado;
+
+
+  },
+
+  decode: function(desplazamiento, texto){
+    var resultado2 = "";
+    var offset = 26-(desplazamiento%26);
+
+    if(desplazamiento == null){
+      throw new TypeError('Not Valid');
+    }
+    else if(texto == []){
+        throw new TypeError('Not Valid');
+      }
+      else if(desplazamiento == 0){
+        throw new TypeError('Not Valid');
+      }
+
+    for(let i=0; i < texto.length; i++){ 
+      let text = texto.charCodeAt(i);  //Pasar texto a ASCII
+
+          if(text >= 65 && text <= 90) {
+            resultado2 += String.fromCharCode((text - 65 + offset) % 26 + 65); //fromCharCode: Convierte de ascii a string
+          console.log(resultado2);
+          }
+          else{
+            resultado2 += texto.charAt(i);
+          }
+    }  
+  return resultado2;
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-function registra(){
-    document.getElementById('content').style.display='none'; //ocultar
-    caja_name.style.display='block';  //mostrar
-}
-
-function showname(){
-    let name;
-    name=document.getElementById("name").value
-    document.getElementById()
-}
-
-
 import cipher from './cipher.js';
 
+    document.getElementById('btn-uno').addEventListener("click", function(){
+        pantallaUno.style.display='none'; //ocultar
+        pantallaDos.style.display='block';  //mostrar
+    });
+
+    document.getElementById('btn-dos').addEventListener("click", function(){
+        pantallaDos.style.display='none'; //ocultar
+        pantallaTres.style.display='block'; //ocultar
+    });
+
+
+    document.getElementById("mensaje1").addEventListener("keyup", function(){
+        mensaje1.value = mensaje1.value.toUpperCase();
+
+    },true);
+    document.getElementById("cifrar").addEventListener("click", function(){
+        var texto = document.getElementById("mensaje1").value; //Para sacar el valor del texto a cifrar
+        var desplazamiento = document.getElementById("desplazamiento").value; //Para sacar el número de desplazamiento
+       // console.log('texto');
+        //document.querySelector("#mensaje2").innerHTML = cipher.encode(texto, parseInt(desplazamiento));
+
+        document.getElementById("mensaje2").innerHTML = cipher.encode(parseInt(desplazamiento), texto); //Para que se escriba en la caja de texto
+    },true);
+
+        document.getElementById("descifrar").addEventListener("click", function(){
+            var texto = document.getElementById("mensaje1").value; //Para sacar el valor del texto a cifrar
+            var desplazamiento = document.getElementById("desplazamiento").value; //Para sacar el número de desplazamiento
+            //console.log('texto');
+            //document.querySelector("#mensaje2").innerHTML = cipher.encode(texto, parseInt(desplazamiento));
+
+            document.getElementById("mensaje2").innerHTML = cipher.decode(parseInt(desplazamiento), texto); //Para que se escriba en la caja de texto
+});
+
 console.log(cipher);
+
